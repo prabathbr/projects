5f54f50644c537976f85c8d5055eccf5445486ec
prabathbr <60564552+prabathbr@users.noreply.github.com>
2022-03-05 03:58:05 +1100

    Speed Test - Python & C++ - Calculating Factors

---
+++ PythonSpeedTest_Factors/SingleThreadCPP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+long long int number = 5000000000;
+
+int main(){
+    auto start = high_resolution_clock::now();
+
+    for(long long int i=1;i <= number; i++){
+        if(number % i == 0){
+            std::cout << i << " is a factor of " << number << std::endl;
+        }
+    }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    std::cout << "\n Execution duration: " << duration.count() << " ms" << std::endl;
+
+    return 0;
+}
---
+++ PythonSpeedTest_Factors/SingleThreadPython.py
@@ -0,0 +1,12 @@
+from time import perf_counter
+
+number = 5000000000
+
+t1_start = perf_counter()
+
+for i in range(1, number+1):
+    if number % i == 0:
+        print(f"{i} is a factor of {number}")
+
+t1_stop = perf_counter()        
+print(f"\n Execution duration: {1000* (t1_stop-t1_start)} ms")
