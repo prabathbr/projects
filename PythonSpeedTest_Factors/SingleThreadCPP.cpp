a76827caaca9f8f02839b29cb4fd64bebc2737a3
prabathbr <60564552+prabathbr@users.noreply.github.com>
2022-03-09 12:00:21 +1100

    Update SingleThreadCPP.cpp

--- PythonSpeedTest_Factors/SingleThreadCPP.cpp
+++ PythonSpeedTest_Factors/SingleThreadCPP.cpp
@@ -7,7 +7,7 @@ long long int number = 5000000000;
 int main(){
     auto start = high_resolution_clock::now();
 
-    for(long long int i=1;i <= long long int(number/2); i++){
+    for(long long int i=1;i <= long long int((number+1)/2); i++){
         if(number % i == 0){
             std::cout << i << " is a factor of " << number << std::endl;
         }
