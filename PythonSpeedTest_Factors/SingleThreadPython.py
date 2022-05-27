c715e75da4a508b45a21ea95ce9b9ed8c5b1aedd
prabathbr <60564552+prabathbr@users.noreply.github.com>
2022-05-27 17:07:19 +1000

    Update SingleThreadPython.py

--- PythonSpeedTest_Factors/SingleThreadPython.py
+++ PythonSpeedTest_Factors/SingleThreadPython.py
@@ -4,7 +4,7 @@ number = 5000000000
 
 t1_start = perf_counter()
 
-for i in range(1, int((number+1)/2)):
+for i in range(1, 1 + int((number+1)/2)):
     if number % i == 0:
         print(f"{i} is a factor of {number}")
 
